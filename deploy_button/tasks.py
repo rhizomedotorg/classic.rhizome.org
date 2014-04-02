7f9d83c7ddac3b02c67aa83710c9acf3d80f460d
Rhizome.org <smeisburger@gmail.com>
2014-04-02 12:10:48 -0400

    updated requirements

--- deploy_button/tasks.py
+++ deploy_button/tasks.py
@@ -1,11 +1,12 @@
-from fabric.api import execute
+# from fabric.api import execute
 from celery import task
 
 
 @task()
 def deploy():
-    try:
-        from fabfile import quick_deploy 
-        return execute(quick_deploy)
-    except Exception as e:
-        return 'my_celery_task -- %s' % e.message
+    # try:
+    #     from fabfile import quick_deploy 
+    #     return execute(quick_deploy)
+    # except Exception as e:
+    #     return 'my_celery_task -- %s' % e.message
+    pass
