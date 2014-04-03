2fd4b30236a7aa8ba28423c920ad849e4044c142
protonpopsicle <smeisburger@gmail.com>
2014-04-03 11:45:09 -0400

    made 7on7 2014 page live

--- sevenonseven/urls.py
+++ sevenonseven/urls.py
@@ -4,9 +4,9 @@ from django.views.generic import TemplateView
 
 
 urlpatterns = patterns('discuss.views',
-    url(r'^$', TemplateView.as_view(template_name='sevenonseven/early.html'), name='sevenonseven_landing'),
+    url(r'^$', TemplateView.as_view(template_name='sevenonseven/2014.html'), name='sevenonseven_landing'),
     url(r'^past/$', TemplateView.as_view(template_name='sevenonseven/landing.html'), name='sevenonseven_past'),
-	url(r'^preview/$',login_required(TemplateView.as_view(template_name='sevenonseven/2014.html'))),
+	#url(r'^preview/$',login_required(TemplateView.as_view(template_name='sevenonseven/2014.html'))),
     url(r'^2010/$', TemplateView.as_view(template_name='sevenonseven/2010.html')),
     url(r'^2011/$', TemplateView.as_view(template_name='sevenonseven/2011.html')),
     url(r'^2012/$', TemplateView.as_view(template_name='sevenonseven/2012.html')),
