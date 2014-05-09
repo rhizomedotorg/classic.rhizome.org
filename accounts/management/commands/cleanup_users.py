87147a1cb6b65e7f86283b678c285660b881cd0c
Rhizome.org <smeisburger@gmail.com>
2014-05-09 11:00:42 -0400

    updated user cleanup script

--- accounts/management/commands/cleanup_users.py
+++ accounts/management/commands/cleanup_users.py
@@ -11,41 +11,36 @@ from accounts.models import RhizomeUser, UserRating
 
 class Command(BaseCommand):
     '''
+    this should be run once
     '''
     
     def handle(*args, **options):
         # innactive (or haven't logged in recently) users with no important associated content?
         
-        delta = datetime.timedelta(days=1000)
+        delta = datetime.timedelta(days=365)
         
-        q = RhizomeUser.objects.annotate(num_artworks=Count('artist/user'),\
-                                         num_exhibitions=Count('user/curator')\
-        ).filter(num_artworks=0, num_exhibitions=0)
+        # these properties can't be filtered in query since names aren't valid
+        q = RhizomeUser.objects.annotate(num_artworks=Count('artist/user'),
+                                         num_exhibitions=Count('user/curator'),
+                                         num_proposals=Count('user who created the proposal'),
+                                         num_orgsubs=Count('the org sub admin')
+        ).filter(num_artworks=0, num_exhibitions=0, num_proposals=0, num_orgsubs=0)
         
-        import pdb; pdb.set_trace()
-
         q = q.filter(comment_comments__isnull=True, event__isnull=True,\
                      opportunity__isnull=True, post__isnull=True,\
-                     member__isnull=True, approvalvote__isnull=True,\
+                     approvalvote__isnull=True,\
                      newdonation__isnull=True, reblogpost__isnull=True,\
                      staffmember__isnull=True, downloadofthemonth__isnull=True,\
-                     job__isnull=True, jobpostingpayment__isnull=True,\
-                     collectioncurator__isnull=True, rankvote__isnull=True)
+                     job__isnull=True, collectioncurator__isnull=True, rankvote__isnull=True)
 
         innactive_users = q.filter(is_active=False)
         dead_users = q.filter(last_login__lte=datetime.datetime.today() - delta, is_active=True)
 
-        users_to_delete = []
+        print 'deleting %s users...' % (innactive_users.count() + dead_users.count())
 
-        for user in innactive_users:
-            users_to_delete.append(user.id)
+        innactive_users.delete()
+        dead_users.delete()
 
-        for user in dead_users:
-            users_to_delete.append(user.id)
+        print 'done.'
 
-        import pdb; pdb.set_trace()
-        # bulk delete
-        #RhizomeUser.objects.filter(id__in=users_to_delete).delete()
-
-            
-
+        
